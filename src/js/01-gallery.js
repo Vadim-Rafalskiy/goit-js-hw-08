7c16df9cbdbc8b14ae4efe1d0ab878f64cbc4740
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2022-12-09 23:40:41 +0200

    1+2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const getItemTemplate = ({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}"><img class="gallery__image" alt="${description}"  src="${preview}"></a>`;
+
+const render = () => {
+  const lis = galleryItems.map(getItemTemplate);
+
+  refs.gallery.innerHTML = '';
+  refs.gallery.insertAdjacentHTML('beforeend', lis.join(''));
+};
+
+render();
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const handleClick = e => {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  e.preventDefault();
+
+  gallery.on('shown.simpleLightbox');
+  refs.gallery.removeEventListener('click', handleClick);
+}; //
+
+refs.gallery.addEventListener('click', handleClick);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+
+const LOCALSTORAGE_CURRENT_TIME = 'videoplayer-current-time';
+
+const getTimeLocalStorage = JSON.parse(
+  localStorage.getItem(LOCALSTORAGE_CURRENT_TIME)
+);
+
+const { seconds: currentTime } = getTimeLocalStorage;
+
+player.setCurrentTime(currentTime);
+
+const onPlay = function (data) {
+  localStorage.setItem(LOCALSTORAGE_CURRENT_TIME, JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
